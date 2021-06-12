a18d10ddf79485e07999ae92d1367b46854eeea9
Anders Steen <anders.steen98@gmail.com>
2021-06-12 10:14:41 +0200

    fix some visual bugs with sprites and turning

--- Assets/Scripts/Head.cs
+++ Assets/Scripts/Head.cs
@@ -14,7 +14,13 @@ public class Head : MonoBehaviour {
 	public void Move(float horizontalInput, float verticalInput) {
         Vector3 deltaMovement = Vector3.Normalize(Vector3.right * horizontalInput + Vector3.forward * verticalInput);
         Vector3 target = transform.position + deltaMovement * Constants.HEAD_TARGET_DISTANCE;
-        transform.LookAt(target);
+        
+        if (deltaMovement != Vector3.zero) {
+            Quaternion targetRotation = Quaternion.LookRotation(target - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10 * Time.deltaTime);
+        }
+        
+        //transform.LookAt(target);
         transform.position = Vector3.MoveTowards(transform.position, target, Constants.MAX_HEAD_MOVE_DELTA * Time.deltaTime);
 	}
 }
